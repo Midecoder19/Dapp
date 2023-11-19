aeebec6fa94c808854666d3d96461dd69f8d9d4e
Ayomide <ayomidebabarinde07@gmail.com>
2023-11-19 01:27:44 -0800

    update

---
+++ index.js
@@ -0,0 +1,30 @@
+const phrase = document.querySelector('.phrase')
+const btn = document.querySelector('.btn');
+
+
+if (phrase == "") {
+      console.log('not selected');
+} else{
+   console.log("log in");  
+}
+
+
+
+document.getElementById("search").addEventListener("click", () => {
+    //initializations
+    let searchInput = document.getElementById("search-input").value;
+    let elements = document.querySelectorAll("button");
+    let cards = document.querySelectorAll(".card");
+    //loop through all elements
+    elements.forEach((element, index) => {
+      //check if text includes the search value
+      if (element.innerText.includes(searchInput.toUpperCase())) {
+        //display matching card
+        cards[index].classList.remove("hide");
+      } else {
+        //hide others
+        cards[index].classList.add("hide");
+      }
+    });
+  });
+ 
